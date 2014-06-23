52ba5f7a738589d37db9a56fe626f53a70c2776c
Justin Gilman <eyeofmidas@gmail.com>
2014-06-23 15:56:17 -0400

    fixed scaling issues with odd aspect ratios

--- core/src/com/matsemann/libgdxloadingscreen/screen/LoadingScreen.java
+++ core/src/com/matsemann/libgdxloadingscreen/screen/LoadingScreen.java
@@ -1,14 +1,20 @@
 package com.matsemann.libgdxloadingscreen.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.matsemann.libgdxloadingscreen.LoadingBar;
 import com.matsemann.libgdxloadingscreen.SomeCoolGame;
 
@@ -17,128 +23,134 @@ import com.matsemann.libgdxloadingscreen.SomeCoolGame;
  */
 public class LoadingScreen extends AbstractScreen {
 
-    private Stage stage;
+	private Stage stage;
 
-    private Image logo;
-    private Image loadingFrame;
-    private Image loadingBarHidden;
-    private Image screenBg;
-    private Image loadingBg;
+	private Image logo;
+	private Image loadingFrame;
+	private Image loadingBarHidden;
+	private Image screenBg;
+	private Image loadingBg;
 
-    private float startX, endX;
-    private float percent;
+	private float startX, endX;
+	private float percent;
 
-    private Actor loadingBar;
+	private Actor loadingBar;
 
-    public LoadingScreen(SomeCoolGame game) {
-        super(game);
-    }
+	public LoadingScreen(SomeCoolGame game) {
+		super(game);
+	}
 
-    @Override
-    public void show() {
-        // Tell the manager to load assets for the loading screen
-        game.manager.load("data/loading.pack", TextureAtlas.class);
-        // Wait until they are finished loading
-        game.manager.finishLoading();
+	@Override
+	public void show() {
+//		stage.setViewport(new FitViewport(800, 480, new PerspectiveCamera()));
 
-        // Initialize the stage where we will place everything
-        stage = new Stage();
+		// Tell the manager to load assets for the loading screen
+		game.manager.load("data/loading.pack", TextureAtlas.class);
+		// Wait until they are finished loading
+		game.manager.finishLoading();
 
-        // Get our textureatlas from the manager
-        TextureAtlas atlas = game.manager.get("data/loading.pack", TextureAtlas.class);
+		// Initialize the stage where we will place everything
+		stage = new Stage();
 
-        // Grab the regions from the atlas and create some images
-        logo = new Image(atlas.findRegion("libgdx-logo"));
-        loadingFrame = new Image(atlas.findRegion("loading-frame"));
-        loadingBarHidden = new Image(atlas.findRegion("loading-bar-hidden"));
-        screenBg = new Image(atlas.findRegion("screen-bg"));
-        loadingBg = new Image(atlas.findRegion("loading-frame-bg"));
+		// Get our textureatlas from the manager
+		TextureAtlas atlas = game.manager.get("data/loading.pack", TextureAtlas.class);
 
-        // Add the loading bar animation
-        Animation anim = new Animation(0.05f, atlas.findRegions("loading-bar-anim") );
-        anim.setPlayMode(PlayMode.LOOP_REVERSED);
-        loadingBar = new LoadingBar(anim);
+		// Grab the regions from the atlas and create some images
+		logo = new Image(atlas.findRegion("libgdx-logo"));
+		loadingFrame = new Image(atlas.findRegion("loading-frame"));
+		loadingBarHidden = new Image(atlas.findRegion("loading-bar-hidden"));
+		screenBg = new Image(atlas.findRegion("screen-bg"));
+		loadingBg = new Image(atlas.findRegion("loading-frame-bg"));
 
-        // Or if you only need a static bar, you can do
-        // loadingBar = new Image(atlas.findRegion("loading-bar1"));
+		// Add the loading bar animation
+		Animation anim = new Animation(0.05f, atlas.findRegions("loading-bar-anim"));
+		anim.setPlayMode(PlayMode.LOOP_REVERSED);
+		loadingBar = new LoadingBar(anim);
 
-        // Add all the actors to the stage
-        stage.addActor(screenBg);
-        stage.addActor(loadingBar);
-        stage.addActor(loadingBg);
-        stage.addActor(loadingBarHidden);
-        stage.addActor(loadingFrame);
-        stage.addActor(logo);
+		// Or if you only need a static bar, you can do
+		// loadingBar = new Image(atlas.findRegion("loading-bar1"));
 
-        // Add everything to be loaded, for instance:
-        // game.manager.load("data/assets1.pack", TextureAtlas.class);
-        // game.manager.load("data/assets2.pack", TextureAtlas.class);
-        // game.manager.load("data/assets3.pack", TextureAtlas.class);
-    }
+		// Add all the actors to the stage
+		stage.addActor(screenBg);
+		stage.addActor(loadingBar);
+		stage.addActor(loadingBg);
+		stage.addActor(loadingBarHidden);
+		stage.addActor(loadingFrame);
+		stage.addActor(logo);
 
-    @Override
-    public void resize(int width, int height) {
-        // Set our screen to always be XXX x 480 in size
-        width = 480 * width / height;
-        height = 480;
-        stage.getViewport().update(width , height, false);
+		// Add everything to be loaded, for instance:
+		// game.manager.load("data/assets1.pack", TextureAtlas.class);
+		// game.manager.load("data/assets2.pack", TextureAtlas.class);
+		// game.manager.load("data/assets3.pack", TextureAtlas.class);
+	}
 
-        // Make the background fill the screen
-        screenBg.setSize(width, height);
+	@Override
+	public void resize(int width, int height) {
+		// Scale the viewport to fit the screen
+		Vector2 scaledView = Scaling.fit.apply(800, 480, width, height);
+		stage.getViewport().update((int)scaledView.x, (int)scaledView.y, true);
 
-        // Place the logo in the middle of the screen and 100 px up
-        logo.setX((width - logo.getWidth()) / 2);
-        logo.setY((height - logo.getHeight()) / 2 + 100);
+		// Make the background fill the screen
+		screenBg.setSize(width, height);
 
-        // Place the loading frame in the middle of the screen
-        loadingFrame.setX((stage.getWidth() - loadingFrame.getWidth()) / 2);
-        loadingFrame.setY((stage.getHeight() - loadingFrame.getHeight()) / 2);
+		// Place the logo in the middle of the screen and 100 px up
+		logo.setX((width - logo.getWidth()) / 2);
+		logo.setY((height - logo.getHeight()) / 2 + 100);
 
-        // Place the loading bar at the same spot as the frame, adjusted a few px
-        loadingBar.setX(loadingFrame.getX() + 15);
-        loadingBar.setY(loadingFrame.getY() + 5);
+		// Place the loading frame in the middle of the screen
+		loadingFrame.setX((stage.getWidth() - loadingFrame.getWidth()) / 2);
+		loadingFrame.setY((stage.getHeight() - loadingFrame.getHeight()) / 2);
 
-        // Place the image that will hide the bar on top of the bar, adjusted a few px
-        loadingBarHidden.setX(loadingBar.getX() + 35);
-        loadingBarHidden.setY(loadingBar.getY() - 3);
-        // The start position and how far to move the hidden loading bar
-        startX = loadingBarHidden.getX();
-        endX = 440;
+		// Place the loading bar at the same spot as the frame, adjusted a few
+		// px
+		loadingBar.setX(loadingFrame.getX() + 15);
+		loadingBar.setY(loadingFrame.getY() + 5);
 
-        // The rest of the hidden bar
-        loadingBg.setSize(450, 50);
-        loadingBg.setX(loadingBarHidden.getX() + 30);
-        loadingBg.setY(loadingBarHidden.getY() + 3);
-    }
+		// Place the image that will hide the bar on top of the bar, adjusted a
+		// few px
+		loadingBarHidden.setX(loadingBar.getX() + 35);
+		loadingBarHidden.setY(loadingBar.getY() - 3);
+		// The start position and how far to move the hidden loading bar
+		startX = loadingBarHidden.getX();
+		endX = 440;
 
-    @Override
-    public void render(float delta) {
-        // Clear the screen
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		// The rest of the hidden bar
+		loadingBg.setSize(450, 50);
+		loadingBg.setX(loadingBarHidden.getX() + 30);
+		loadingBg.setY(loadingBarHidden.getY() + 3);
+	}
 
-        if (game.manager.update()) { // Load some, will return true if done loading
-            if (Gdx.input.isTouched()) { // If the screen is touched after the game is done loading, go to the main menu screen
-                game.setScreen(new MainMenuScreen(game));
-            }
-        }
+	@Override
+	public void render(float delta) {
+		// Clear the screen
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Interpolate the percentage to make it more smooth
-        percent = Interpolation.linear.apply(percent, game.manager.getProgress(), 0.1f);
+		if (game.manager.update()) { // Load some, will return true if done
+										// loading
+			if (Gdx.input.isTouched()) { // If the screen is touched after the
+											// game is done loading, go to the
+											// main menu screen
+				game.setScreen(new MainMenuScreen(game));
+			}
+		}
 
-        // Update positions (and size) to match the percentage
-        loadingBarHidden.setX(startX + endX * percent);
-        loadingBg.setX(loadingBarHidden.getX() + 30);
-        loadingBg.setWidth(450 - 450 * percent);
-        loadingBg.invalidate();
+		// Interpolate the percentage to make it more smooth
+		percent = Interpolation.linear.apply(percent, game.manager.getProgress(), 0.1f);
 
-        // Show the loading screen
-        stage.act();
-        stage.draw();
-    }
+		// Update positions (and size) to match the percentage
+		loadingBarHidden.setX(startX + endX * percent);
+		loadingBg.setX(loadingBarHidden.getX() + 30);
+		loadingBg.setWidth(450 - 450 * percent);
+		loadingBg.invalidate();
 
-    @Override
-    public void hide() {
-        // Dispose the loading assets as we no longer need them
-        game.manager.unload("data/loading.pack");
-    }
+		// Show the loading screen
+		stage.act();
+		stage.draw();
+	}
+
+	@Override
+	public void hide() {
+		// Dispose the loading assets as we no longer need them
+		game.manager.unload("data/loading.pack");
+	}
 }
--- desktop/src/com/matsemann/libgdxloadingscreen/desktop/DesktopLauncher.java
+++ desktop/src/com/matsemann/libgdxloadingscreen/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.matsemann.libgdxloadingscreen.SomeCoolGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "libgdx-loading-screen";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new SomeCoolGame(), config);
 	}
 }
